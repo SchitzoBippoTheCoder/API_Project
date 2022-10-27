ed2d6b3f2f34ce1e353e5f21ab696c1e08292ab5
Samuel Soon <samuelsoon36@gmail.com>
2022-10-27 12:47:56 -0400

    Finished project using TMDB API

--- script.js
+++ script.js
@@ -2,20 +2,32 @@
 // in --> objects
 // of --> array
 
+//const { default: axios } = require("axios");
+
 const apiKey = "e06cb446302dcf3a3cb1358720141aad";
 
-let movieName = prompt("Search for a movie:");
+function GetMovieData() {
+    let movieID = parseInt(document.getElementById("dropdownMenu").value);
 
-let search = axios.get("https://api.themoviedb.org/3/search/movie", {
-    params: {
-        api_key: apiKey,
-        query: movieName,
-    }
-})
+    let search = axios.get("https://api.themoviedb.org/3/movie/" + movieID, {
+        params: {
+            api_key: apiKey,
+            append_to_response: "videos",
+        }
+    })
 
-let searchResults = search.then((moviesData) => {
-    console.table(moviesData.data.results);
-    console.log(moviesData.data.results[1].original_title)
-});
+    let searchResult = search.then((movieData) => {
+        document.getElementById("movieImage").style.backgroundImage = `url(https://image.tmdb.org/t/p/w500/${movieData.data.backdrop_path})`;
+        document.getElementById("poster").src = "https://image.tmdb.org/t/p/w500" + movieData.data.poster_path;
 
-console.log(searchResults);
+        document.getElementById("title").innerHTML = movieData.data.original_title;
+        document.getElementById("tagline").innerHTML = movieData.data.tagline;
+        document.getElementById("status").innerHTML = `${movieData.data.status} ~ ${movieData.data.release_date}`;
+        document.getElementById("popularity").innerHTML = `Popularity: ${movieData.data.popularity}`;
+        document.getElementById("voteAverage").innerHTML = `Vote Average: ${movieData.data.vote_average}`;
+        document.getElementById("voteCount").innerHTML = `Vote Count: ${movieData.data.vote_count}`;
+        document.getElementById("budget").innerHTML = `Budget: $${movieData.data.budget}`;
+        document.getElementById("overview").innerHTML = movieData.data.overview;
+        document.getElementById("trailer").src = "https://www.youtube.com/embed/" + (movieData.data.videos.results.filter((trailer) => trailer.type === "Trailer")).at(0).key;
+    })
+}
