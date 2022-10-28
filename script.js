258233a407a8d0e308907ad171539053af924f30
Samuel Soon <samuelsoon36@gmail.com>
2022-10-27 21:52:35 -0400

    Added a search bar to add results into dropdown menu

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 // in --> objects
 // of --> array
 
-//const { default: axios } = require("axios");
-
 const apiKey = "e06cb446302dcf3a3cb1358720141aad";
 
 function GetMovieData() {
@@ -29,5 +27,29 @@ function GetMovieData() {
         document.getElementById("budget").innerHTML = `Budget: $${movieData.data.budget}`;
         document.getElementById("overview").innerHTML = movieData.data.overview;
         document.getElementById("trailer").src = "https://www.youtube.com/embed/" + (movieData.data.videos.results.filter((trailer) => trailer.type === "Trailer")).at(0).key;
+    })
+}
+
+function Search() {
+    let searchParam = document.getElementById("searchBar").value;
+
+    let _searchParam = axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${searchParam}`, {
+
+    })
+
+    let _searchParamResult = _searchParam.then((finalResult) => {
+        //console.log(finalResult.data.results[0]);
+
+        document.getElementById("dropdownMenu").innerText = null;
+
+        for (let count = 0; count <= finalResult.data.results.length; count++) {
+            let newOption = document.createElement("option");
+            newOption.setAttribute('value', finalResult.data.results[count].id);
+            newOption.innerHTML = finalResult.data.results[count].original_title;
+
+            console.log(newOption.innerHTML);
+
+            document.getElementById("dropdownMenu").options.add(newOption);
+        }
     })
 }
